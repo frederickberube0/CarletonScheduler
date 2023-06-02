af860e06770d59c5543d68862e29ec613561d01d
frederickberube <frederickberube0@gmail.com>
2023-06-01 22:07:38 -0400

    getSpecialization

--- getCalendars.py
+++ getCalendars.py
@@ -2,10 +2,12 @@ import requests, re, json
 from bs4 import BeautifulSoup as bs
 
 def main():
-    print(getPrograms())
+    for obj in getPrograms():
+        for specialization in getSpecializations(obj):
+            makeTablesFromSpecialization(specialization)
 
 def getPrograms() -> list[dict[str, str]]:
-    """This function return a list of objects which contain program title and its url"""
+    """This function return a list of programs in dictionary form which contain program title and its url"""
     program_request = requests.get('https://calendar.carleton.ca/undergrad/undergradprograms/')
     parsed_major_request = bs(program_request.content, 'html.parser').find_all(class_="page_content")
     
@@ -28,8 +30,33 @@ def getPrograms() -> list[dict[str, str]]:
     return list_of_programs
     #print(list(parsed_major_request)[0].split("\n"))
 
+def getSpecializations(program: dict[str, str]) -> list[dict[str, str]]:
+    """Takes a an program (COMP, BUSI) object in form {course, url} and returns it's specialization in form {link, program_name, url}"""
+    #/undergrad/undergradprograms/undeclared/
+    
+    url = "https://calendar.carleton.ca" + program["url"]
+    program_request = requests.get(url)
+    
+    #Page content
+    program_request_page_content = bs(program_request.content, 'html.parser').find_all(id="textcontainer")
+    specialization_list = bs(str(program_request_page_content[0]), 'html.parser').find_all('li')
 
-def getSpecializations(list_of_program_object: list[dict[str, str]]):
-    pass
+    object_list = []
+    for specialization in specialization_list:
+        #print(line)
+        parsed_line = bs(str(specialization), 'html.parser')
+        link = parsed_line.find('a')['href']
+        program_name = parsed_line.find('a').text
+        result = {'link': link, 'program_name': program_name, 'page_content': program_request_page_content}
+        object_list.append(result)
+
+    return object_list
+
+def makeTablesFromSpecialization(obj: dict[str, str]):
+    """Takes a specialization object in form {link, program_name, page_content} and write to file"""
+    print(obj["page_content"])
+
+
+
 
 main()
