17d692153ffc9b15b78ca7b409c01d9c0a3b2314
Your Name <frederickberube0@gmail.com>
2023-06-06 19:51:34 -0400

    start of gettimetable

--- getTimeTable.py
+++ getTimeTable.py
@@ -1,2 +1,11 @@
 #https://central.carleton.ca/prod/bwysched.p_select_term?wsea_code=EXT
 #https://github.com/uScheduleMe/uoapi
+#https://central.carleton.ca/prod/bwysched.p_search_fields?wsea_code=EXT&session_id=2&term_code=202320
+
+import requests
+
+def getTimetable(course: str, term_code: str):
+    program_request = requests.get('https://central.carleton.ca/prod/bwysched.p_search_fields?wsea_code=EXT&session_id=2&term_code='+term_code)
+    print(str(program_request.content))
+
+getTimetable("", "202320")
